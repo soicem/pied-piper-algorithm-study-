a8fa34e0f7db47e0f8e28e6716c64f18ee9794b3
kim namgue <knq1130@naver.com>
2018-03-17 23:31:15 +0900

    oil.c complete

--- 1week/2. fri/soicem/3.oil/oil.c
+++ 1week/2. fri/soicem/3.oil/oil.c
@@ -2,59 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int checkOil(int left, int right) {
+int min = 0;
+int max = 0;
+
+void checkOil(int left, int right) {
 	int pivot = (left + right) / 2;
 	int v1;
 	int v2;
-	int ret = 0;
+
 	v1 = observe(left, pivot);
 	v2 = observe(pivot + 1, right);
 	if (v1 == 1) {
-		//printf("%d %d\n", left, pivot);
-		ret += 1 << left;
-		ret += 1 << pivot;
+		printf("v1 == %d %d\n", left, pivot);
+		
+		if (min == 0 || min > left)
+			min = left;
+		if (max == 0 || max < pivot)
+			max = pivot;
 	}
 	if (v2 == 1) {
-		//printf("%d %d\n", pivot + 1, right);
-		ret += 1 << (pivot + 1);
-		ret += 1 << (right);
+		printf("v2 == 1, %d %d\n", pivot + 1, right);
+		if (min == 0 || min > pivot + 1)
+			min = pivot + 1;
+		if (max == 0 || max < right)
+			max = right;
 	}
 
 	if (v1 == 2) {
 		if (v2 > 0)
 			left += 1;
-		ret += checkOil(left, pivot);
+		checkOil(left, pivot);
 	}
 	if (v2 == 2) {
 		if (v1 > 0)
 			right -= 1;
-		ret += checkOil(pivot + 1, right);
+		checkOil(pivot + 1, right);
 		
 	}
-	return ret;
 }
 
 int main() {
 	initial();
 	int N = oil_size();
-	int val = checkOil(0, N - 1);
-	int min = 0;
-	int max = 0;
+	
+
 	int sVal = 1;
 	int cnt = 0;
 	int tf = 1;
-	while (val > sVal) {
-		sVal = 1 << cnt;
-		
-		if(tf)
-			if (val & sVal) {
-				min = cnt;
-				tf = 0;
-			}
-		cnt++;
-	}
-	max = cnt - 2; // cnt++로 1증가와 초과분 1을 빼줘야한다.
-	oil_report(2, 11);
+	checkOil(0, N - 1);
+
+	oil_report(min, max);
 	system("pause");
 	return 0;
 }
